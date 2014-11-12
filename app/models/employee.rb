58747fb0103f023a267f1ae11f36b2e6bc1b7fe2
varg90 <kuhn.alexey@gmail.com>
2014-11-12 19:49:23 +0800

    add employee contacts validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
   validates :full_name, presence: true, format: {with: /[а-яА-Я]+\s+[а-яА-Я]+\s+[а-яА-Я]+/}
+  validates :contacts, presence: true, format: {
+      with: /(8|\+7)?([ .-]?)\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})|(\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z)/i
+  }
 end
