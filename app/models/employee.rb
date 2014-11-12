3f4ea5fa4b9634b59c8486cc575a8d213a2dfa23
varg90 <kuhn.alexey@gmail.com>
2014-11-12 19:54:04 +0800

    add employee salary validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,5 @@ class Employee < ActiveRecord::Base
   validates :contacts, presence: true, format: {
       with: /(8|\+7)?([ .-]?)\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})|(\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z)/i
   }
+  validates :salary, numericality: {greater_than_or_equal_to: 0}
 end
